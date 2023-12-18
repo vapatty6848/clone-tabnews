a3d82dd21040936f97549805ef9af605763bb318
vapatty6848 <vapatty@gmail.com>
2023-12-18 16:39:24 -0300

    add ´variaveis de ambiente´

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
+import database from "../../../../infra/database.js"
 
-
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;")
+  console.log(result.rows)
   response.status(200).json({chave:  "Somos acima da média"})
 }
 export default status;
