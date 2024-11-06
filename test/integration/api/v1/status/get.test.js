81fed854ff7622704c25ff21ed248c44a3ea999b
vapatty6848 <vapatty@gmail.com>
2024-11-06 15:45:23 -0300

    finish `api/v1/status` endpoint

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,12 +1,14 @@
 
-
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200)
+  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
 
-  const parseUpdateAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toEqual(parseUpdateAt)
-})
+ const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.postgres_version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
