6b2475ebb52a004581b2ee58e5f3ec36d0ebdc62
vapatty6848 <vapatty@gmail.com>
2024-11-06 15:41:37 -0300

    make database.js more robust to error handling

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,32 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString();
-  response.status(200).json({
-    update_at: updateAt,
+  const updatedAt = new Date().toISOString();
+  const dataBaseName = process.env.POSTGRES_DB;
 
-  })
-  console.log(updateAt)
+ const dataPostgresVersion = await database.query("SHOW server_version;");
+  const postgresVersion = dataPostgresVersion.rows[0].server_version;
+
+  const dataMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = dataMaxConnections.rows[0].max_connections;
+
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName],
+  });
+
+  const openedConnections = databaseOpenedConnections.rows[0].count;
+
+  const result = response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
+
 export default status;
