97049c65c90aeafe7c8d0658dc8d6c64d54dd56b
vapatty6848 <33123681+vapatty6848@users.noreply.github.com>
2025-01-22 19:24:31 +0000

    add migrations e comentarios e os testes de get e post antes da refatoraçao

--- infra/database.js
+++ infra/database.js
@@ -7,8 +7,16 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-   ssl: getSSLValues(),
+    ssl: getSSLValues(),
   });
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  })
 
   try {
      await client.connect();
@@ -20,16 +28,24 @@ async function query(queryObject) {
     await client.end();
   }
 }
+
 export default {
   query: query,
 };
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
+
     return {
       ca: process.env.POSTGRES_CA,
     }
   }
 
-  return process.env.NODE_ENV ==="development" ? false : true
+ /*  testando que o jest esta em ambient de test e next em desenvolvimento 
+  console.log("NODE_ENV: "+  process.env.NODE_ENV);
+  return process.env.NODE_ENV ==="production" ? true : false */
+
+  return process.env.NODE_ENV ==="production" ? true : false
 }
+
+
---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path:".env.development",
+});
+
+const nextJest = require("next/jest");
+
+
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>" ],
+});
+
+
+module.exports = jestConfig;
+
+
+/* o jest só ira carregar as variaveis de ambiente apos configurar o dropIndex, mas 
+neste caso nossa variavel esta em .env.develop, então precisamos indicar o caminho */
