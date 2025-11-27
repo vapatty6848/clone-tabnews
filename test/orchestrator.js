63c1c78623822fb1f84479df593072d6eb172384
vapatty6848 <vapatty@gmail.com>
2025-11-27 10:04:57 -0300

    adds  GitHub Actions

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -1,26 +1,26 @@
 import retry from "async-retry";
 
 
-async function waitForAllServices () {
+async function waitForAllServices() {
   await waitForWebServer()
 
 
-  async function waitForWebServer(){
+  async function waitForWebServer() {
     return retry(fetchStatusPage, {
-      retries: 100,  
+      retries: 100,
+      maxTimeout: 1000,
     });
 
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
-    
-    }
-
-  
+      //const responseBody = await response.json();
+      if (response.status !== 200) {
+        throw Error();
       }
+    }
+  }
 }
-
 export default {
   waitForAllServices,
 };
